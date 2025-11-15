5f6b754c603b7bc6b145e61429b97c3f8299ee43
minhvu <84697874+mivud@users.noreply.github.com>
2025-11-15 11:48:48 +0700

    upgrade .NET 10

---
+++ src/LightConsole/DependencyInjection.cs.cs
@@ -0,0 +1,24 @@
+﻿using LightConsole.Extensions;
+using LightConsole.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LightConsole;
+
+public static class ServiceCollections
+{
+    /// <summary>
+    ///     Register DI when run console.
+    /// </summary>
+    public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration config)
+    {
+        // manual inject services here
+        services.AddTransient<IDateTimeService, DateTimeService>();
+        services.AddTransient<IConfigurationService, ConfigurationService>();
+
+        // auto inject services
+        services.AutoAddServices();
+
+        return services;
+    }
+}
